4df8ee5b7071cded91b987654e733054b7997714
cjjst123 <30718032+cjjst123@users.noreply.github.com>
2018-09-11 21:14:41 -0700

    Update DiffEqua.m

--- HW_3/DiffEqua.m
+++ HW_3/DiffEqua.m
@@ -1,1 +1,38 @@
-d
+%   This question is about discreting the interval and apply numerical analysis to solve equation for certain points.
+%
+%   Here are two critical pinciples:
+%   (1)  f'[n]= (f[n+1] - f[n-1]) / (2*h)
+%   (2)  f"[n]= (f[n+1] - 2*f[n] + f[n-1]) / h^2
+%
+%   By applying numerical analysis, we can easily create function. For f"(x) - 2*f'(x) = cosx,
+%   let x = xn, The equation can be written as:
+%
+%          (1/h^2+1/h , 1/h^2 ,1/h^2-1/h) * (x[n-1],x[n],x[n+1]) = cos(xn)
+%
+%    For x = xn.
+%    Then we can create sparse matrix.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+n = 2000;           %Number of dots
+l = 0;              %Left boundary
+r = pi;             %Right boundary
+vl = 0;             %f(l)
+vr = -1;            %f(r)
+h =(r-l)/(n-1);     %Step for equation
+
+A = sparse(n,n);        %Target matrix
+b = zeros(n,1);         %Right-hand-side value
+x = linspace(l,r,n);    %Split interval to calculate 
+
+b([1 n]) = [vl vr];     %For left and right, assigning initial value
+b(2:n-1) = cos(x(2:n-1));       %Create differential equation
+
+A(1,1) = 1;                     %For both-side, assigning cof 1
+A(n,n) = 1;
+
+for i=2:n-1
+    A(i,i-1:i+1) = [1/h^2+1/h , -2/h^2 1/h^2-1/h];          %Differential equation
+end
+
+fx = A \ b;
+plot(x,fx);
