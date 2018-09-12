d13cd51b6e6628217178ac5f6bf3240d02a0a25a
cjjst123 <30718032+cjjst123@users.noreply.github.com>
2018-09-11 21:04:38 -0700

    Update HW_3_DiffEquation.m

--- HW_3_DiffEquation.m
+++ HW_3_DiffEquation.m
@@ -1,4 +1,4 @@
-<script type="text/javascript" async src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML"> </script>
+<img src="http://chart.googleapis.com/chart?cht=tx&chl=\Large x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}" style="border:none;">
 %   This question is about discreting the interval and apply numerical analysis to solve equation for certain points.
 %
 %
