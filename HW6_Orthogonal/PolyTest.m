f2ec0a91c4924fb73a6192b0f564f96a778ffaa5
cjjst123 <30718032+cjjst123@users.noreply.github.com>
2018-10-07 21:18:14 -0700

    Update PolyTest.m

--- HW6_Orthogonal/PolyTest.m
+++ HW6_Orthogonal/PolyTest.m
@@ -1,16 +1,16 @@
 % PolyTest.m
-pol = cell(1,4);
-pol{1} = [1];
+pol = cell(1,4);                    % Create pointer container for matrix
+pol{1} = [1];                       % Initializing four polynomials as matrices 
 pol{2} = sqrt(3)*[-1 2];
 pol{3} = sqrt(5)*[1 -6 6];
 pol{4} = sqrt(7)*[-1 12 -30 20];
 
-a = zeros(4,4);
+a = zeros(4,4);                     % Correlation matrix, where a_{ij} means the inner product of pol_i and pol_j 
 
 for i=1:4
     for j= 1:4
         a(i,j) = polydot(pol{i},pol{j});
     end
 end
-a(abs(a)<1e-8) = 0;
+a(abs(a)<1e-8) = 0;                 % Error tolerance. This elimiates results such as -0.00
 disp(a);
