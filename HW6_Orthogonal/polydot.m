7f5f21723bc5bb9269f6471a713df283ed82af2c
cjjst123 <30718032+cjjst123@users.noreply.github.com>
2018-10-07 21:11:16 -0700

    Update polydot.m

--- HW6_Orthogonal/polydot.m
+++ HW6_Orthogonal/polydot.m
@@ -1,9 +1,10 @@
-function [res] = polydot(x,y)
+%  polydot.m
+function [res] = polydot(x,y)   % Input two matrix (i.e. 1,2x,3x^2... as [1 2 3]) as argu 
     
-    mul = conv(x,y);
-    n = size(mul,2);
+    mul = conv(x,y);            % Polydot is equavalent to discrete convolution
+    n = size(mul,2);            % Get the size of product
     
-    intcof = 1./(1:n);
-    res = intcof*mul';
+    intcof = 1./(1:n);          
+    res = intcof*mul';          % Directly take matrix multiplication as integral( [1 2 3] * [1 1/2 1/3]')
 end
 
