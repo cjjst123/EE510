d27e01a3e5ed373f731da98724881214f54bb606
cjjst123 <30718032+cjjst123@users.noreply.github.com>
2018-11-07 21:14:59 -0800

    Update f.m

--- f.m
+++ f.m
@@ -1,4 +1,4 @@
-% min_eig
+% max_eig
 n = 100;
 A = rand(n,n);
 A = diag(diag(A)) + triu(A,1) + triu(A,1)';
@@ -7,7 +7,7 @@ x = rand(n,1);                      % Randomly generate a vector x
 epoch = 300;                        % Epoch of iteration
 
 for i = 1:epoch                     % Power iteration
-    x = A \  x;
+    x = A *  x;
     x = x / norm(x);
 end
 
@@ -16,7 +16,7 @@ eigenval = x'*A*x;                  % Calculate eigenvalue(if truely convergent)
 try
     assert(sum(A*x - eigenval*x) < 1e-10)   % Check if AX = λx by summing the difference
     fprintf('Check sucess!\n')
-    fprintf('The least-magnitude eigenvalue is %.6f.\n',eigenval);
+    fprintf('The largest-magnitude eigenvalue is %.6f.\n',eigenval);
     disp(x');
 catch
     fprintf('The iteration is not convergent\n')
