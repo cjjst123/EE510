3ec57f529bb6e1fcabe1d258c6184b964b5fe62c
cjjst123 <30718032+cjjst123@users.noreply.github.com>
2018-11-07 20:54:45 -0800

    Update f.m

--- f.m
+++ f.m
@@ -1,34 +1,21 @@
-% mapower.m
-A = [0 -3 -2;2 5 2;-2 -3 0];                % Matrix A
+n = 100;
+A = rand(n,n);
+A = diag(diag(A)) + triu(A,1) + triu(A,1)';
 
-u = zeros(3,3);                             % Initialize eigenvectors
-eigen = zeros(1,3);
-epoch = 200;
+x = rand(n,1);                      % Randomly generate a vector x
+epoch = 300;                        % Epoch of iteration
 
-for i = 1:2                                 % Find the biggest two eigenvecors w.r.t largest eigenvalue
-    x = rand(3,1);
-    for j = 1:epoch
-        for k=1:i-1
-            x = x - u(:,k)' * x * u(:,k);   % Gram-Smith method
-        end
-        x = A*x;                            % Amplify the component
-        x = x/norm(x);                      % normalization        
-    end
-    u(:,i) = x;                             % Normalized eigenvector       
-    eigen(i) = x'*A*x;                      % Save the eigenvalue
-end
-
-inv_A = inv(A);                             % Use inverse to find smallest eigenvalue
-x = rand(3,1);
-for j = 1:epoch
-    x = inv_A*x;
+for i = 1:epoch                     % Power iteration
+    x = A*x;
     x = x / norm(x);
 end
-u(:,3) = x;
-eigen(3) = 1/(x'*A*x);                      % It's the reciprocal of minimun eigenvalue
-fprintf('Eigenvectors are :\n')
-disp(u);
-fprintf('Eigenvalues are: \n')
-disp(eigen);
-fprintf('Check convergency:\n Au ./ u =\n')
-disp(A*u./u)
+
+eigenval = x'*A*x;                  % Calculate eigenvalue(if truely convergent)
+
+try
+    assert(sum(A*x - eigenval*x) < 1e-10)       % Check if AX = λx by summing the difference
+    fprintf('Check sucess!\n')
+catch
+    fprintf('The iteration is not convergent')
+end
+fprintf('The largest-magnitude eigenvalue is %.6f.\n',eigenval);
